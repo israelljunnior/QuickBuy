0a1f05ac5a361b6734f0245108b023f4d4757b69
Israel Junior <israelljunnior15987426@hotmail.com>
2020-02-18 23:20:41 -0300

    Primeira Migração e CargaInicial para FormaPAgamento

---
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.ObjetosDeValor;
+using QuickBuy.Repositorio.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+    public class QuickBuyContexto : DbContext
+    {
+        
+        DbSet<Usuario> Usuarios { get; set; }
+        DbSet<Produto> Produtos { get; set; }
+        DbSet<Pedido> Pedidos { get; set; }
+        DbSet<ItemPedido> ItemPedidos { get; set; }
+        DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+        public QuickBuyContexto(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+            modelBuilder.ApplyConfiguration(new ItemPedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new FormaPagamentoConfiguration());
+
+            modelBuilder.Entity<FormaPagamento>().HasData(
+                new FormaPagamento()
+                {
+                    Id = 1,
+                    Nome = "Boleto",
+                    Descricao = "Forma de Pagamento Boleto"
+                },
+                new FormaPagamento()
+                {
+                    Id = 2,
+                    Nome = "Cartao de Crédito",
+                    Descricao = "Forma de Pagamento Cartão de Crédito"
+                },
+                new FormaPagamento()
+                {
+                    Id = 3,
+                    Nome = "Depósito",
+                    Descricao = "Forma de Pagamento Depósito"
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}

e89e39ceb8b074bde5b010f0ff92f6a34b5b38de
Israel Junior <israelljunnior15987426@hotmail.com>
2020-03-03 10:42:32 -0300

    Adicionando Controller de Usuário

---
+++ QuickBuy.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickBuy.Web.Controllers
+{
+
+    [Route("/api/[controller]")]
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;   
+        }
+
+        [HttpPost("VerificarUsuario")]
+        public ActionResult VerificarUsuario([FromBody] Usuario usuario)
+        {
+            try {
+                var usuarioRetorno = _usuarioRepositorio.Obter(usuario.Email, usuario.Senha);
+                if (usuarioRetorno != null)
+                    return Ok(usuario);
+                return BadRequest("Usuario ou Senha Inválidos");
+            } catch(Exception ex) {
+                return BadRequest(ex.ToString()); 
+            }
+        }
+        [HttpPost]
+        public ActionResult Post()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+    }
+}

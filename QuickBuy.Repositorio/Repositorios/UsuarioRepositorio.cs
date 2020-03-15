46cf71992d0c22df4e04d5ea56649c2536f652f7
Israel Junior <israelljunnior15987426@hotmail.com>
2020-03-15 18:32:36 -0300

    adição método de buscar de usuário por email

--- QuickBuy.Repositorio/Repositorios/UsuarioRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/UsuarioRepositorio.cs
@@ -18,5 +18,9 @@ namespace QuickBuy.Repositorio.Repositorios
         {
             return this.QuickBuyContexto.Usuarios.FirstOrDefault(u => u.Email == email && u.Senha == senha);
         }
+        public Usuario Obter(string email)
+        {
+            return this.QuickBuyContexto.Usuarios.FirstOrDefault(u => u.Email == email);
+        }
     }
 }

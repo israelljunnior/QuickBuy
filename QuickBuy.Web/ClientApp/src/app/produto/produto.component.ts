004859db85ee4fdb5e03e9e38745e58ebd246695
Israel Junior <israelljunnior15987426@hotmail.com>
2020-03-15 18:29:37 -0300

    produto component

--- QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProdutoService } from '../servicos/produto/produto.service';
+import { Produto } from '../modelo/produto.model';
 
 @Component({
   selector: "produto",
-  template: "<html><body>{{ obterNome() }}</body></html>"
+  templateUrl: "./produto.component.html"
 })
-export class ProdutoComponent {
-  public nome: string;
-  public liberadoParaVenda: boolean;
+export class ProdutoComponent implements OnInit {
 
-  obterNome(): string {
-    return "Samsung";
-  };
+  private produto;
+  
+  constructor(private produtoService: ProdutoService) {
+
+  }
+
+  ngOnInit() {
+    this.produto = new Produto();
+  }
+
+  cadastrar() {
+    this.produtoService.cadastrar(this.produto).subscribe(
+      produtoJson => {
+        console.log(produtoJson);
+      },
+      e => {
+        console.log(e.error);
+      }
+    );
+  }
+
 }
